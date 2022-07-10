a780cb680f7524d81c0425367a995e891bce669c
megarciamo <meli.garcia614@gmail.com>
2022-07-09 23:02:59 -0500

    Segunda entrega

--- js/main.js
+++ js/main.js
@@ -6,6 +6,60 @@ class Producto {
   }
 }
 
+class Carrito {
+  constructor(nombre, cantidad, precio) {
+    this.nombre = nombre;
+    this.cantidad = cantidad;
+    this.precio = precio;
+  }
+}
+
+let nombreUsuario
+
+document.getElementById("formulario-usuario").addEventListener("submit", manejadorFormularioUsuario)
+
+function manejadorFormularioUsuario(e) {
+    e.preventDefault()
+    nombreUsuario = document.getElementById("user").value
+
+    let ComprasPendientes = document.getElementById("ComprasPendientes")
+    const pendientes = JSON.parse(localStorage.getItem(nombreUsuario))
+
+    console.log(pendientes)
+
+    if (pendientes == null || pendientes.length == 0) {
+       ComprasPendientes.innerHTML = "<h1>No hay compras pendientes</h1>"
+    } else {
+      const CP = document.getElementById("CP")
+
+      CP.innerHTML =
+      `<h3>Bienvenido ${nombreUsuario}, a continuación puedes ver tus compras pendientes</h3>`;
+        mostrarPendientes(pendientes)
+        
+    }
+}
+
+
+function mostrarPendientes(pendientes) {
+  let ComprasPendientes = document.getElementById("ComprasPendientes")
+  ComprasPendientes.innerHTML = ""
+
+  pendientes.forEach(pendiente => {
+      let li = document.createElement("li")
+      li.innerHTML = `
+       ${pendiente.nombre} - ${pendiente.cantidad} - ${pendiente.precio}`
+      const botonBorrar = document.createElement("button")
+      botonBorrar.innerText = "Borrar"
+      botonBorrar.addEventListener("click", () => {
+          eliminarpendiente(pendiente)
+      })
+      li.appendChild(botonBorrar)
+      ComprasPendientes.appendChild(li)
+  })   
+}
+
+
+
 let total = 0;
 
 const producto1 = new Producto("./img/Pintura.PNG", "Pintura", 100);
@@ -21,36 +75,56 @@ productos.push(producto3);
 function catalogo(productos) {
   const contenedorProductos = document.getElementById("contenedor-productos");
   contenedorProductos.innerHTML = "";
-  productos.forEach((producto) => {
+  productos.forEach((producto, i) => {
     const divProducto = document.createElement("div");
     divProducto.classList.add("card");
     divProducto.innerHTML = `
         <img src="${producto.imagen}" alt="${producto.nombre}">
         <div>
-            <p>${producto.nombre}</p>
-            <p>${producto.precio}</p>
-        </div>
-          `;
-    const botonAgregarCarrito = document.createElement("button");
-    botonAgregarCarrito.innerText = "Agregar al carrito";
-    botonAgregarCarrito.classList.add("addToCart");
-    botonAgregarCarrito.addEventListener("click", () => {
-      total += producto.precio;
-      verTotal();
-    });
 
-    divProducto.appendChild(botonAgregarCarrito);
+        <form id="cantidad-alcarrito${i}">
+        <p id="nombre${i}">${producto.nombre}</p>
+        <p id="precio${i}"> $ ${producto.precio}</p>
+    <input type="text" id="cantidad${i}" placeholder="Cantidad">
+    <button type="submit">Agregar al carito</button>
+    </form>`;
+
+
 
     contenedorProductos.appendChild(divProducto);
+
+    document.getElementById(`cantidad-alcarrito${i}`).addEventListener("submit", function(event){
+      agregarCarrito(i, event)
+    })
   });
 }
 
-function verTotal() {
-  const h2total = document.getElementById("total");
-  h2total.innerHTML = "";
-  h2total.innerHTML = `
-        El valor de su compra es: ${total} 
-        `;
+function agregarCarrito(i, e) {
+  e.preventDefault()
+  const nombre = document.getElementById(`nombre${i}`).innerHTML
+  const cantidad = document.getElementById(`cantidad${i}`).value
+  const precio = document.getElementById(`precio${i}`).innerHTML
+  const carrito = new Carrito (nombre, cantidad, precio)
+
+const itemsCarritoLocalStorage = JSON.parse(localStorage.getItem(nombreUsuario))
+if (itemsCarritoLocalStorage == null) {
+  localStorage.setItem(nombreUsuario, JSON.stringify([carrito]))
+  mostrarPendientes([carrito])
+}else {
+  itemsCarritoLocalStorage.push(carrito)
+  localStorage.setItem(nombreUsuario, JSON.stringify(itemsCarritoLocalStorage))
+  mostrarPendientes(itemsCarritoLocalStorage)
 }
+e.target.reset()
+}
+
+function eliminarpendiente(carrito) {
+  const itemsCarritoLocalStorage = JSON.parse(localStorage.getItem(nombreUsuario))
+  const nuevoArray = itemsCarritoLocalStorage.filter(item => item.nombre != carrito.nombre)
+  localStorage.setItem(nombreUsuario, JSON.stringify(nuevoArray))
+  mostrarPendientes(nuevoArray)
+}
+
+
 
 catalogo(productos);
