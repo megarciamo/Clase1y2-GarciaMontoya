a326b0490e0c96c78403de20dccb052407830922
megarciamo <meli.garcia614@gmail.com>
2022-07-23 21:43:33 -0500

    Fetch

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+
+alert("Ingrese un nombre de usuario para que la página funcione correctamente")
+
 class Producto {
   constructor(imagen, nombre, precio) {
     this.imagen = imagen;
@@ -57,95 +60,6 @@ function mostrarPendientes(pendientes) {
   });
 }
 
-let total = 0;
-
-const producto1 = new Producto("./img/Pintura.PNG", "Pintura", 100);
-const producto2 = new Producto("./img/Brocha.PNG", "Brocha", 50);
-const producto3 = new Producto("./img/Thinner.PNG", "Thinner", 30);
-
-const productos = [];
-
-productos.push(producto1);
-productos.push(producto2);
-productos.push(producto3);
-
-// const {imagen, nombre, precio} = producto
-
-function catalogo(productos) {
-  const contenedorProductos = document.getElementById("contenedor-productos");
-  contenedorProductos.innerHTML = "";
-  productos.forEach((producto, i) => {
-    const divProducto = document.createElement("div");
-    divProducto.classList.add("card");
-    divProducto.innerHTML = `
-        <img src="${producto.imagen}" alt="${producto.nombre}">
-        <div>
-
-        <form id="cantidad-alcarrito${i}">
-        <p id="nombre${i}">${producto.nombre}</p>
-        <p id="precio${i}"> $ ${producto.precio}</p>
-    <input type="number" id="cantidad${i}" placeholder="Cantidad">
-    <button type="submit">Agregar al carito</button>
-    </form>`;
-
-    contenedorProductos.appendChild(divProducto);
-
-    document
-      .getElementById(`cantidad-alcarrito${i}`)
-      .addEventListener("submit", function (event) {
-        agregarCarrito(i, event) });
-  });
-}
-
-function agregarCarrito(i, e) {
-  e.preventDefault();
-
-  const nombre = document.getElementById(`nombre${i}`).innerHTML;
-  const cantidad = document.getElementById(`cantidad${i}`).value;
-  const precio = document.getElementById(`precio${i}`).innerHTML;
-  const carrito = new Carrito(nombre, cantidad, precio);
-
-  if (carrito.cantidad == "" || carrito.cantidad.trim() == "") {
-    
-Swal.fire({
-  icon: 'error',
-  title: 'Oops...',
-  text: 'El valor no puede estar vacio !',
-  footer: '<a href="">Why do I have this issue?</a>'
-})
-  } else {
-
-  const itemsCarritoLocalStorage = JSON.parse(
-    localStorage.getItem(nombreUsuario)
-  );
-  
-    if (itemsCarritoLocalStorage == null) {
-    localStorage.setItem(nombreUsuario, JSON.stringify([carrito]));
-    mostrarPendientes([carrito]);
-  } else if (itemsCarritoLocalStorage.find((item) => item.nombre === carrito.nombre)) {
-    const result = itemsCarritoLocalStorage.find((item) => item.nombre === carrito.nombre);
-    carrito.cantidad = parseInt(carrito.cantidad) + parseInt(result.cantidad);
-
-    const nuevoArray1 = itemsCarritoLocalStorage.filter(
-      (item) => item.nombre != carrito.nombre
-    );
-
-    nuevoArray1.push(carrito);
-
-    localStorage.setItem(nombreUsuario, JSON.stringify(nuevoArray1));
-    mostrarPendientes(nuevoArray1);
-  } else {
-    itemsCarritoLocalStorage.push(carrito);
-    localStorage.setItem(
-      nombreUsuario,
-      JSON.stringify(itemsCarritoLocalStorage)
-    );
-    mostrarPendientes(itemsCarritoLocalStorage);
-  }
-  e.target.reset();
-}
-}
-
 function eliminarpendiente(carrito) {
   Swal.fire({
     title: "Estás seguro?",
@@ -171,4 +85,107 @@ function eliminarpendiente(carrito) {
   });
 }
 
-catalogo(productos);
+let total = 0;
+
+
+const URL = 'js/data/products.json'
+
+
+fetch ( URL )
+  .then( res => res.json() )
+  .then (data => { 
+    const producto6 = new Producto(data[0].imagen, data[0].nombre, data[0].precio);
+    const producto7 = new Producto(data[1].imagen, data[1].nombre, data[1].precio);
+    const producto8 = new Producto(data[2].imagen, data[2].nombre, data[2].precio);
+    
+    const productos = [];
+
+    productos.push(producto6);
+    productos.push(producto7);
+    productos.push(producto8);
+
+    function catalogo(productos) {
+      const contenedorProductos = document.getElementById("contenedor-productos");
+      contenedorProductos.innerHTML = "";
+      productos.forEach((producto, i) => {
+        const divProducto = document.createElement("div");
+        divProducto.classList.add("card");
+        divProducto.innerHTML = `
+            <img src="${producto.imagen}" alt="${producto.nombre}">
+            <div>
+    
+            <form id="cantidad-alcarrito${i}">
+            <p id="nombre${i}">${producto.nombre}</p>
+            <p id="precio${i}"> $ ${producto.precio}</p>
+        <input type="number" id="cantidad${i}" placeholder="Cantidad">
+        <button type="submit">Agregar al carito</button>
+        </form>`;
+    
+        contenedorProductos.appendChild(divProducto);
+    
+        document
+          .getElementById(`cantidad-alcarrito${i}`)
+          .addEventListener("submit", function (event) {
+            agregarCarrito(i, event) });
+      });
+    }
+    
+    function agregarCarrito(i, e) {
+      e.preventDefault();
+    
+      const nombre = document.getElementById(`nombre${i}`).innerHTML;
+      const cantidad = document.getElementById(`cantidad${i}`).value;
+      const precio = document.getElementById(`precio${i}`).innerHTML;
+      const carrito = new Carrito(nombre, cantidad, precio);
+    
+     
+      if (carrito.cantidad == "" || carrito.cantidad.trim() == "") {
+        
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'El valor no puede estar vacio !',
+      footer: '<a href="">Why do I have this issue?</a>'
+    })
+      } else {
+    
+      const itemsCarritoLocalStorage = JSON.parse(
+        localStorage.getItem(nombreUsuario)
+      );
+      
+        if (itemsCarritoLocalStorage == null) {
+        localStorage.setItem(nombreUsuario, JSON.stringify([carrito]));
+        mostrarPendientes([carrito]);
+      } else if (itemsCarritoLocalStorage.find((item) => item.nombre === carrito.nombre)) {
+        const result = itemsCarritoLocalStorage.find((item) => item.nombre === carrito.nombre);
+        carrito.cantidad = parseInt(carrito.cantidad) + parseInt(result.cantidad);
+    
+        const nuevoArray1 = itemsCarritoLocalStorage.filter(
+          (item) => item.nombre != carrito.nombre
+        );
+    
+        nuevoArray1.push(carrito);
+    
+        localStorage.setItem(nombreUsuario, JSON.stringify(nuevoArray1));
+        mostrarPendientes(nuevoArray1);
+      } else {
+        itemsCarritoLocalStorage.push(carrito);
+        localStorage.setItem(
+          nombreUsuario,
+          JSON.stringify(itemsCarritoLocalStorage)
+        );
+        mostrarPendientes(itemsCarritoLocalStorage);
+      }
+      e.target.reset();
+    }
+    }
+    
+    catalogo(productos);
+
+    })
+
+   
+
+
+
+
